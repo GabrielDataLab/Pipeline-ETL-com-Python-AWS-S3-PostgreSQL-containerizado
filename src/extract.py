6f4f2b64f19440067218dcfaf59e1af138249939
Gabriel Henrique <gabriel-gabries@hotmail.com>
2026-04-28 17:45:00 -0300

    pegando os dados das 2 fontes

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,8 @@
 import requests
-requisicao = requests.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json&dataInicial=01/01/2024&dataFinal=28/04/2026')
-print(requisicao)
-print(requisicao.json())           
+requisicao_selic = requests.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados?formato=json&dataInicial=01/01/2024&dataFinal=28/04/2026')
+print(requisicao_selic)
+print(requisicao_selic.json())
+
+requisicao_ipca = requests.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial=01/01/2024&dataFinal=28/04/2026')
+print(requisicao_ipca)
+print(requisicao_ipca.json())
