695d8b2e668fc8fd62d9176e3f9c7459c1b34ff0
Gabriel Henrique <gabriel-gabries@hotmail.com>
2026-04-28 21:09:47 -0300

    orquestrador principal do pipeline

---
+++ main.py
@@ -0,0 +1,34 @@
+import logging
+import os
+from src.extract import fetch_serie, save_local, upload_to_s3
+from src.load import load
+from src.transform import transform
+
+logging.basicConfig(
+                    level=logging.INFO,
+                    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s " 
+                        )
+logger = logging.getLogger(__name__)
+
+BUCKET = "pipelinebcb"
+data_inicial = '01/01/2024'
+data_final = '28/04/2026'
+
+SERIES = [
+    {"codigo": 433, "nome": "ipca"},
+    {"codigo": 432, "nome": "selic"}
+]
+
+def run():
+    logger.info("Pipeline iniciado")
+    for serie in SERIES:
+        logger.info(f"Processando serie {serie["nome"]}")
+        dados_brutos = fetch_serie(serie["codigo"], data_inicial, data_final)
+        dados_transformados = transform(dados_brutos, serie["nome"])
+        caminho = save_local(dados_brutos,serie["nome"])
+        s3_key = os.path.relpath(caminho).replace("\\", "/")
+        upload_to_s3(caminho, 'pipelinebcb', s3_key)
+        load(dados_transformados)
+    logger.info("Pipeline finalizado")
+if __name__ == "__main__":
+    run()
