cadc47ae859d85da188faa6f447c5e72b543cd41
Gabriel Henrique <gabriel-gabries@hotmail.com>
2026-05-30 19:37:08 -0300

    fix: implementa upsert real com ON CONFLICT e corrige uso da variável BUCKET

--- src/load.py
+++ src/load.py
@@ -12,15 +12,22 @@ def load(data: list) -> None:
         user=os.getenv("DB_USER"), 
         password=os.getenv("DB_PASSWORD"),
         )
-    con.autocommit = True
     cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS indicadores_bcb(
-                id SERIAL PRIMARY KEY,
-                data_referencia DATE NOT NULL,
-                valor FLOAT NOT NULL,
-                serie VARCHAR(50) NOT NULL 
-                )""")
-    cur.executemany("INSERT INTO indicadores_bcb (data_referencia, valor, serie) VALUES (%s, %s, %s)", [(item["data_referencia"], item["valor"], item["serie"]) for item in data])
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS indicadores_bcb (
+        id SERIAL PRIMARY KEY,
+        data_referencia DATE NOT NULL,
+        valor FLOAT NOT NULL,
+        serie VARCHAR(50) NOT NULL,
+        UNIQUE (data_referencia, serie)
+    )
+""")
+    cur.executemany("""
+    INSERT INTO indicadores_bcb (data_referencia, valor, serie)
+    VALUES (%s, %s, %s)
+    ON CONFLICT (data_referencia, serie)
+    DO UPDATE SET valor = EXCLUDED.valor
+""", [(item["data_referencia"], item["valor"], item["serie"]) for item in data])
     con.commit()
     logging.info(f"{len(data)} registros inseridos na tabela indicadores_bcb")
     cur.close()
